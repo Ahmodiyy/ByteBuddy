8375a4ed9a47b2cddac23a6b7bb4360523da21c5
Ahmodiyy <ahmodolaitan03@gmail.com>
2024-05-15 13:46:36 +0100

    onboarding integration test

--- integration_test/main_test.dart
+++ integration_test/main_test.dart
@@ -1,21 +1,27 @@
-import 'package:bytebuddy/features/onboarding/presentation/view/onboarding.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:bytebuddy/main.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'auth/login_robot.dart';
 import 'auth/register_robot.dart';
 import 'onboarding/onboarding_robot.dart';
+import 'package:bytebuddy/firebase_options.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> main() async {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-  testWidgets("whole app", (WidgetTester tester) async {
+  testWidgets("app test", (WidgetTester tester) async {
     // Arrange
     await tester.binding.setSurfaceSize(const Size(500, 800));
+
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
     User? user = FirebaseAuth.instance.currentUser;
-    await tester.pumpWidget(MyApp(user));
+    await tester.pumpWidget(ProviderScope(child: MyApp(user)));
 
     if (user == null) {
       OnboardingRobot onboardingRobot = OnboardingRobot(tester);
